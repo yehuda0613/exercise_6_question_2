7bc09c1ff5454ff0f053ec9020efa4e9655fa559
Yoni <yonatankleinse@gmail.com>
2022-05-09 14:46:10 +0300

    Yoni

---
+++ cmake-build-debug/CMakeFiles/exercise_6_question_2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise_6_question_2.dir/List.cpp.obj"
+  "CMakeFiles/exercise_6_question_2.dir/List.cpp.obj.d"
+  "CMakeFiles/exercise_6_question_2.dir/RoundList.cpp.obj"
+  "CMakeFiles/exercise_6_question_2.dir/RoundList.cpp.obj.d"
+  "CMakeFiles/exercise_6_question_2.dir/main.cpp.obj"
+  "CMakeFiles/exercise_6_question_2.dir/main.cpp.obj.d"
+  "exercise_6_question_2.exe"
+  "exercise_6_question_2.exe.manifest"
+  "exercise_6_question_2.pdb"
+  "libexercise_6_question_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exercise_6_question_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/exercise_6_question_2.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for exercise_6_question_2.
+# This may be replaced when dependencies are built.
